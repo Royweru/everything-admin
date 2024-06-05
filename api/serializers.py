45966f1834c37f36c18157813cb50a025353ff90
Royweru <royezi17@gmail.com>
2024-06-05 10:55:55 +0300

    new file:   .gitignore 	modified:   api/serializers.py 	modified:   backend/urls.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model= User
-        fields =["id","username","password"]
-        extra_Kwargs = {"password":{"write_only":True}}
-        
+        model = User
+        fields = ["id", "username", "password"]
+        extra_Kwargs = {"password": {"write_only": True}}
+
     def create(self, validated_data):
-        user= User.objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
         return user
