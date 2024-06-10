c30e707a77c9a7736fb7cd5dc1e8f908dfdbbfca
Royweru <royezi17@gmail.com>
2024-06-10 21:23:46 +0300

    modified:   api/__pycache__/models.cpython-311.pyc 	modified:   api/__pycache__/serializers.cpython-311.pyc 	new file:   api/migrations/0001_initial.py 	new file:   api/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   api/models.py 	modified:   api/serializers.py 	modified:   db.sqlite3

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.postgres.fields import ArrayField
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=1080)
+    slug = models.CharField(max_length=150)
+    price = models.FloatField()
+    featured = models.BooleanField(default=False)
+    # images = ArrayField(models.CharField(max_length=2000),
+    #                     blank=True, default=list)
+    thumbnail = models.CharField(max_length=2000)
